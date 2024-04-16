107422767445581dcbfae2e98d2b167272e1ac5e
Gavin Johnson <gjohnson25@murraystate.edu>
2024-04-16 11:20:15 -0500

    Added Subject to Observer interface.

--- src/main/java/com/example/demo/Observer.java
+++ src/main/java/com/example/demo/Observer.java
@@ -2,11 +2,9 @@ package com.example.demo;
 
 public interface Observer {
     //Source: https://www.digitalocean.com/community/tutorials/observer-design-pattern-in-java
-    //Methods to register and unregister observer
-    public void register(Observer obj);
-    public void unregister(Observer obj);
-    //Method to notify observers of change.
-    public void notifyObserver();
-    //Method to get update from observed subject (i.e. observed object).
-    public void update(int points);
+
+    //Method to update the observer, sued by subect.
+    public void update();
+    //attach with subject to observe
+    public void setSubject(Subject sub);
 }
