7453f933ecf1785543a3008d517aa6677a2759ab
Gavin Johnson <gjohnson25@murraystate.edu>
2024-04-16 11:17:00 -0500

    Built on Observer interface for game scores.

--- src/main/java/com/example/demo/Observer.java
+++ src/main/java/com/example/demo/Observer.java
@@ -1,5 +1,12 @@
 package com.example.demo;
 
 public interface Observer {
-    void update(int points);
+    //Source: https://www.digitalocean.com/community/tutorials/observer-design-pattern-in-java
+    //Methods to register and unregister observer
+    public void register(Observer obj);
+    public void unregister(Observer obj);
+    //Method to notify observers of change.
+    public void notifyObserver();
+    //Method to get update from observed subject (i.e. observed object).
+    public void update(int points);
 }
